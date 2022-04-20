a869022648e09e0f7c2a49f69837be4b27e3cd9f
tangjie133 <1334875503@qq.com>
2022-04-20 08:55:31 +0800

    V1.0.1

--- python/raspberry/examples/read_data.py
+++ python/raspberry/examples/read_data.py
@@ -10,27 +10,28 @@
   @date        2021-08-31
   @url         https://github.com/DFRobot/DFRobot_EnvironmentalSensor
 '''
+from __future__ import print_function
 import sys
 import os
+sys.path.append("../")
 import time
 import RPi.GPIO as GPIO
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-from dfrobot_environmental_sensor import *
+from DFRobot_Environmental_Sensor import *
 
 '''
   Select communication mode
   ctype=1：UART
   ctype=0：IIC
 '''
-ctype=1
+ctype=0
 
 ADDRESS = 0x22  
 I2C_1   = 0x01               
 if ctype==0:
-  SEN0500/SEN0501 = DFRobot_Environmental_Sensor_I2C(I2C_1 ,ADDRESS)
+  SEN050X = DFRobot_Environmental_Sensor_I2C(I2C_1 ,ADDRESS)
 else:
-  SEN0500/SEN0501 = DFRobot_Environmental_Sensor_UART(9600, ADDRESS)
+  SEN050X = DFRobot_Environmental_Sensor_UART(9600, ADDRESS)
 
 '''
   Atmospheric pressure unit select
@@ -45,7 +46,7 @@ TEMP_C                    = 0X03
 TEMP_F                    = 0X04
  
 def setup():
-  while (SEN0500/SEN0501.begin() == False):
+  while (SEN050X.begin() == False):
     print("Sensor initialize failed!!")
     time.sleep(1)
   print("Sensor  initialize success!!")
@@ -53,13 +54,13 @@ def setup():
 def loop():
   ##Obtain sensor data
   print("-----------------------\r\n")
-  print("Temp: " + str(SEN0500/SEN0501.get_temperature(TEMP_C)) + " 'C\r\n")
-  print("Temp: " + str(SEN0500/SEN0501.get_temperature(TEMP_F)) + " 'F\r\n")
-  print("Humidity: " + str(SEN0500/SEN0501.get_humidity()) + " %\r\n")
-  print("Ultraviolet intensity: " + str(SEN0500/SEN0501.get_ultraviolet_intensity()) + " mw/cm2\r\n")
-  print("LuminousIntensity: " + str(SEN0500/SEN0501.get_luminousintensity()) + " lx\r\n")
-  print("Atmospheric pressure: " + str(SEN0500/SEN0501.get_atmosphere_pressure(HPA)) + " hpa\r\n")
-  print("Elevation: " + str(SEN0500/SEN0501.get_elevation()) + " m\r\n")
+  print("Temp: " + str(SEN050X.get_temperature(TEMP_C)) + " 'C\r\n")
+  print("Temp: " + str(SEN050X.get_temperature(TEMP_F)) + " 'F\r\n")
+  print("Humidity: " + str(SEN050X.get_humidity()) + " %\r\n")
+  print("Ultraviolet intensity: " + str(SEN050X.get_ultraviolet_intensity()) + " mw/cm2\r\n")
+  print("LuminousIntensity: " + str(SEN050X.get_luminousintensity()) + " lx\r\n")
+  print("Atmospheric pressure: " + str(SEN050X.get_atmosphere_pressure(HPA)) + " hpa\r\n")
+  print("Elevation: " + str(SEN050X.get_elevation()) + " m\r\n")
   print("-----------------------\r\n")
   time.sleep(1)
 
